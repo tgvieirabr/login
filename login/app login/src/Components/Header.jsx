b8161d69653c3926b959782368f89a3e43e9fa26
tgvieirabr <tgvieirabr@gmail.com>
2023-06-23 15:31:54 -0300

    :tada:

---
+++ login/app login/src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Mdc } from '../Assets/mdc.ico';
+import { UserContext } from '../UserContext';
+
+const Header = () => {
+  const { data } = React.useContext(UserContext);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Mdc - Home">
+          <Mdc />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
