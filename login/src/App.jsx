de3be2d6c1b07534d8db8517e85c41d1ac3a4679
tgvieirabr <tgvieirabr@gmail.com>
2023-06-27 11:09:32 -0300

    add components, and api config

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import './App.css';
+import Api from './api/Api';
+import Login from './Components/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
-
-const App = ( ) => {
-  return <div>App React</div>
-
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
+
 
 export default App;
