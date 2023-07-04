8444bc584ce0224a2d90052ef8a81274ba8a86c6
Gustavo Weber <weber1.caxias@gmail.com>
2023-07-04 17:01:54 -0300

    loading ok

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -11,7 +11,7 @@ const UserHeader = () => {
     const { pathname } = location;
     switch (pathname) {
       case '/conta/postar':
-        setTitle('Poste Sua Foto');
+        setTitle('Criar Arte');
         break;
       case '/conta/estatisticas':
         setTitle('Estatísticas');
