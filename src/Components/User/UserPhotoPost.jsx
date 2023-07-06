96ae9bf52638939fb30d8ff03cf4d086521521b8
Gustavo Weber <weber1.caxias@gmail.com>
2023-07-06 14:53:17 -0300

    .

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserPhotoPost.module.css';
 import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
